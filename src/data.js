824935fa4b19994223ea9be1df9d44c422675216
Osama Housien <trgv83@durham.ac.uk>
2021-10-19 22:12:04 +0100

    Refactored projects

--- src/data.js
+++ src/data.js
@@ -1,294 +1,205 @@
+var webgl={
+  id: 1,
+  title: "WebGl 3D room",
+  img:
+    "assets/webgl.png",
+  link: "https://github.com/OKH6/WebGl_3DRoom",
+  tags: ["WebGL","JS","Shaders"],
+  content: ()=>{
+    return(
+      <div>
+      <hr className="hr"/>
+      <h1>WebGl 3D room</h1>
+      <hr className="hr"/>
+      </div>
+
+    )
+
+  }
+
+}
+
+var OpenMP=    {
+  id: 2,
+  title: "OpenMP optimasation",
+  img:
+    "https://www.lifewire.com/thmb/at_-WtOLCLO4bXOm7NUoS3LbZZE=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-952011128-fb298c0df6324b52be07b9b13ad31cb8.jpg",
+    link: "https://github.com/OKH6/WebGl_3DRoom",
+    tags: ["C++","OpenMP","ODEs","Multithreding"],
+    content: ()=>{
+      return(
+        <div>
+        <hr className="hr"/>
+        <h1>OpenMP optimasation</h1>
+        <hr className="hr"/>
+        </div>
+  
+      )
+  
+    }
+
+}
 
 
+
+var Atari= {
+  id: 3,
+  title: "Atari Gravitar Agent",
+  img:
+    "https://miro.medium.com/max/4918/1*Y0TDuXNyywjqqr5l5GkMQQ.png",
+  link: "https://github.com/OKH6/WebGl_3DRoom",
+  tags: ["Game","Rainforcement learning","PDQN"],
+
+}
+
+var CovidFighter=    {
+  id: 4,
+  title: "Covid fighter game",
+  img:
+    "assets/maskon.png",
+  link: "https://github.com/OKH6/WebGl_3DRoom",
+  tags: ["C#","Unity","Blender","Animation"],
+
+}
+
+
+
+var LatexComp=    {
+  id: 5,
+  title: "Latex file compressor",
+  img:
+    "assets/compress.jpg",
+  link: "https://github.com/OKH6/WebGl_3DRoom",
+  tags: ["PPM","Arithmatic Coding","Python"],
+
+}
+
+var Board=    {
+  id: 6,
+  title: "Bulletin Board",
+  img:
+    "assets/bulletin.jpg",
+  link: "https://github.com/OKH6/WebGl_3DRoom",
+  tags: ["Python","TCP"],
+
+}
+
+
+var ChangingLives=    {
+  id: 7,
+  title: "Changing lives",
+  img:
+    "assets/changing lives.png",
+  link: "https://github.com/OKH6/ChangingLives",
+  tags: ["JS","CSS","Node.JS","BootStrap"],
+
+}
+
+var ObjrctDet=    {
+  id: 8,
+  title: "Mobile object detection",
+  img:
+    "assets/obj.jpg",
+  link: "https://github.com/OKH6/WebGl_3DRoom",
+  tags: ["Tenserflow Light","C++","Android"],
+
+
+  }
+
+
+  var JustHungry=    {
+    id: 9,
+    title: "Just Hungry",
+    img:
+      "assets/distributed.jpg",
+    link: "https://github.com/OKH6/WebGl_3DRoom",
+    tags: ["Python","Distributed systems","API"],
+
+  }
+
+  var LogicParser=    {
+    id: 10,
+    title: "First order Logic parser",
+    img:
+      "assets/comp.jpg",
+    link: "https://github.com/OKH6/WebGl_3DRoom",
+    tags: ["Python","Math","Logic","Compiler Design"],
+  }
+
+
+  var Conway=    {
+    id: 11,
+    title: "Conway's Game Of Life",
+    img:
+      "assets/gol.jpg",
+    link: "https://github.com/OKH6/WebGl_3DRoom",
+    tags: ["C","Game","Math"],
+
+  }
+
+
+
+  var PersonalWeb=    {
+    id: 12,
+    title: "Personal Website",
+    img:
+      "assets/pweb.png",
+    link: "https://github.com/OKH6/WebGl_3DRoom",
+    tags: ["React","JS","SCSS"],
+
+  }
+
+
+
+  var BluetoothMes=    {
+    id: 13,
+    title: "Bluetooth messaging app",
+    img:
+      "assets/Blue.jpg",
+    link: "https://github.com/OKH6/WebGl_3DRoom",
+    tags: ["sdsd","dsf"],
+
+  }
 export const featuredPortfolio = [
-    {
-      id: 1,
-      title: "WebGl 3D room",
-      img:
-        "assets/webgl.png",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["WebGL","JS","Shaders"],
+    webgl,
+    OpenMP,
+    CovidFighter,
+    Atari,
 
-    },
-    {
-      id: 2,
-      title: "OpenMP optimasation",
-      img:
-        "https://www.lifewire.com/thmb/at_-WtOLCLO4bXOm7NUoS3LbZZE=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-952011128-fb298c0df6324b52be07b9b13ad31cb8.jpg",
-        link: "https://github.com/OKH6/WebGl_3DRoom",
-        tags: ["C++","OpenMP","ODEs","Multithreding"],
-
-    },
-    {
-      id: 3,
-      title: "Atari Gravitar Agent",
-      img:
-        "https://miro.medium.com/max/4918/1*Y0TDuXNyywjqqr5l5GkMQQ.png",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["Game","Rainforcement learning","DeepQ"],
-
-    },
-    {
-      id: 4,
-      title: "Covid fighter game",
-      img:
-        "assets/maskon.png",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["C#","Unity","Blender","Animation"],
-
-    },
-    {
-      id: 5,
-      title: "Latex file compressor",
-      img:
-        "assets/compress.jpg",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["PPM","Arithmatic Coding","Python"],
-
-    },
-    {
-      id: 6,
-      title: "Bulletin Board",
-      img:
-        "assets/bulletin.jpg",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["Python","TCP"],
-
-    },
-    {
-      id: 7,
-      title: "Changing lives",
-      img:
-        "assets/changing lives.png",
-      link: "https://github.com/OKH6/ChangingLives",
-      tags: ["JS","CSS","Node.JS","BootStrap"],
-
-    },
-    {
-      id: 8,
-      title: "Mobile object detection",
-      img:
-        "assets/obj.jpg",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["Tenserflow Light","C++","Android"],
-
-
-      },
-
-
-
-
-    {
-      id: 9,
-      title: "Just Hungry",
-      img:
-        "assets/distributed.jpg",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["Python","Distributed systems","API"],
-
-    },
-    {
-      id: 10,
-      title: "First order Logic parser",
-      img:
-        "assets/comp.jpg",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["Python","Math","Logic","Compiler Design"],
-    },
-    {
-      id: 11,
-      title: "Conway's Game Of Life",
-      img:
-        "assets/gol.jpg",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["C","Game","Math"],
-
-    },
-    {
-      id: 12,
-      title: "Personal Website",
-      img:
-        "assets/pweb.png",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["React","JS","SCSS"],
-
-    },
-    {
-      id: 13,
-      title: "Bluetooth messaging app",
-      img:
-        "assets/Blue.jpg",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["sdsd","dsf"],
-
-    },
+    LatexComp,
+    Board,
+    ChangingLives,
+    ObjrctDet,
+    JustHungry,
+    LogicParser,
+    Conway,
+    PersonalWeb,
+    //BluetoothMes,
   ];
   
   export const webPortfolio = [
-    {
-      id: 1,
-      title: "Personal Website",
-      img:
-        "assets/pweb.png",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["React","JS","SCSS"],
-
-    },
-
-    {
-      id: 2,
-      title: "Just Hungry",
-      img:
-        "assets/distributed.jpg",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["Python","Distributed systems","API"],
-
-    },
-    {
-      id: 3,
-      title: "First order Logic parser",
-      img:
-        "assets/comp.jpg",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["Python","Math","Logic","Compiler Design"],
-    },
-    {
-      id: 4,
-      title: "Conway's Game Of Life",
-      img:
-        "assets/gol.jpg",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["C","Game","Math"],
-
-    },
-
-    {
-      id: 5,
-      title: "WebGl 3D room",
-      img:
-        "assets/webgl.png",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["WebGL","JS","Shaders"],
-
-
-      
-    },
-    {
-      id: 6,
-      title: "OpenMP optimasation",
-      img:
-        "https://www.lifewire.com/thmb/at_-WtOLCLO4bXOm7NUoS3LbZZE=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-952011128-fb298c0df6324b52be07b9b13ad31cb8.jpg",
-        link: "https://github.com/OKH6/WebGl_3DRoom",
-        tags: ["C++","OpenMP","ODEs","Multithreding"],
-
-    },
-    {
-      id: 7,
-      title: "Rainforcement learning",
-      img:
-        "https://miro.medium.com/max/4918/1*Y0TDuXNyywjqqr5l5GkMQQ.png",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["Game","Markov Chains","DeepQ"],
-
-    },
-    {
-      id: 8,
-      title: "Covid fighter game",
-      img:
-        "assets/maskon.png",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["C#","Unity","Blender","Animation"],
-
-    },
-    {
-      id: 9,
-      title: "Latex file compressor",
-      img:
-        "assets/compress.jpg",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["PPM","Arithmatic Coding","Python"],
-
-    },
-    {
-      id: 10,
-      title: "Bulletin Board",
-      img:
-        "assets/bulletin.jpg",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["Python","TCP"],
-
-    },
-    {
-      id: 11,
-      title: "Changing lives",
-      img:
-        "assets/changing lives.png",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["JS","CSS","Node.JS","BootStrap"],
-
-    },
-
-    
-
-
+    JustHungry,
+    LogicParser,
+    Conway,
+    PersonalWeb,
+    webgl,
+    OpenMP,
+    Atari,
+    CovidFighter,
+    LatexComp,
+    Board,
+    ChangingLives,
 
 
   ];
   
   export const mobilePortfolio = [
-    
-      {
-      id: 1,
-      title: "Mobile object detection",
-      img:
-        "assets/obj.jpg",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["Tenserflow Light","C++","Android"],
+    ObjrctDet,
 
-
-      },
-
-    {
-      id: 2,
-      title: "Bluetooth messaging app",
-      img:
-        "assets/Blue.jpg",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["sdsd","dsf"],
-
-    },
+    BluetoothMes,
   
 
   ];
   
-  export const designPortfolio = [
 
-
-    {
-      id: 1,
-      title: "WebGl 3D room",
-      img:
-        "assets/webgl.png",
-      link: "https://github.com/OKH6/WebGl_3DRoom",
-      tags: ["WebGL","JS","Shaders"],
-      content: ()=>{
-        return(
-          <div>
-          <hr className="hr"/>
-          <h1>saFDSAGDFAGS</h1>
-          <hr className="hr"/>
-          </div>
-
-        )
-
-      }
-
-      
-    },
-  ];
-  
-  export const contentPortfolio = [
-
-
-  ];
   
